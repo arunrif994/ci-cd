b1facb97ae2ef98d6c969423bfe0cf6e1dbb13df
Arun Prabhu <arunrif994@gmail.com>
2020-04-16 23:56:29 +0530

    semantic release

---
+++ release.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  branches: "master",
+  repositoryUrl: "https://github.com/arunrif994/ci-cd/pull/4",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/github",
+    "@semantic-release/release-notes-generator",
+  ],
+};
